57e20549357896ee87a16fbb5c8b178082da4a48
Rishabh Jain <rishabhjainfinal@gmail.com>
2023-10-13 12:07:28 +0530

    updated

---
+++ src/components/Nav.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="flex items-center justify-between ">
+      <Image
+        className="w-40 h-12 lg:w-72 lg:h-16 "
+        alt="main logo"
+        src="/logo.png"
+        width={275}
+        height={65}
+        priority
+      />
+      <div className="hidden gap-3 fcc lg:gap-12 lg:text-2xl font-base md:flex">
+        <Link href={"#about"} className="text-violet-50">
+          About
+        </Link>
+        <Link href={"#features"} className="text-violet-50">
+          Features
+        </Link>
+        <Link href={"#pricing"} className="text-violet-50">
+          Pricing
+        </Link>
+      </div>
+      <div className="flex items-center justify-center gap-2 lg:gap-8 ">
+        <div className="font-medium text-center lg:text-2xl text-neutral-200">
+          Sign in
+        </div>
+        <div className="font-medium text-center lg:text-2xl text-neutral-200">
+          Sign up
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/components/sections/hero.tsx
@@ -0,0 +1,91 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function HeroSection() {
+  return (
+    <section className="fcb min-h-[55vh] my-10 justify-center md:gap-20 gap-8 md:justify-between  px-auto w-full fc md:fr">
+      {/* text  */}
+      <div className="flex flex-col justify-between h-full gap-8  md:max-w-[35em] w-full ">
+        <div className="  flex-col justify-start items-start gap-[2rem] flex  ">
+          <div className="text-neutral-200 text-[1.5rem] font-semibold md:text-[3rem]">
+            Build Your Professional
+            <br />
+            Resume with Simple Login
+          </div>
+          <div className="flex flex-col items-start justify-between p-4 ">
+            <div className="flex items-start justify-start gap-1 ">
+              <Image
+                className="w-[22px] h-[17px]"
+                alt="arrow svg"
+                src="/svgs/arrow.svg"
+                width={22}
+                height={17}
+              />
+              <div className=" text-zinc-400 text-[17px] font-medium">
+                Find Your Starting Point, Craft Your Perfect Resume
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-1 ">
+              <Image
+                className="w-[22px] h-[17px]"
+                alt="arrow svg"
+                src="/svgs/arrow.svg"
+                width={22}
+                height={17}
+              />
+              <div className=" text-zinc-400 text-[17px] font-medium">
+                Master the Art of Resume Writing with Best Practices
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-1 ">
+              <Image
+                className="w-[22px] h-[17px]"
+                alt="arrow svg"
+                src="/svgs/arrow.svg"
+                width={22}
+                height={17}
+              />
+              <div className=" text-zinc-400 text-[17px] font-medium">
+                Unlock Success with Mentor-Recommended Templates
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-1 ">
+              <Image
+                className="w-[22px] h-[17px]"
+                alt="arrow svg"
+                src="/svgs/arrow.svg"
+                width={22}
+                height={17}
+              />
+              <div className=" text-zinc-400 text-[17px] font-medium">
+                Get Noticed by HR - Make Your Resume Irresistible
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-1 ">
+              <Image
+                className="w-[22px] h-[17px]"
+                alt="arrow svg"
+                src="/svgs/arrow.svg"
+                width={22}
+                height={17}
+              />
+              <div className=" text-zinc-400 text-[17px] font-medium">
+                Rise Above the Rest - Stand Out from Your Peers
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* button */}
+        <div className="p-4 px-16 rounded-lg bg-gradient-to-r from-blue-600 to-fuchsia-500 border-stone-500 w-fit">
+          <div className="text-center text-neutral-200 md::text-[2.3rem] font-medium">
+            Build Your Resume
+          </div>
+        </div>
+      </div>
+
+      {/* images  */}
+    </section>
+  );
+}
+
+export default HeroSection;
